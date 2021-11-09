d1cf65a188f82af0ffa4c0568b32a60deca19807
aziz306 <80386670+aziz306@users.noreply.github.com>
2021-11-09 09:29:11 +0100

    Add files via upload

---
+++ CRUD/mainwindow.cpp
@@ -0,0 +1,133 @@
+   #include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "employe.h"
+#include <QMessageBox>
+#include <QIntValidator>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+  ui->lineEdit_Id->setValidator( new QIntValidator(0, 9999, this));
+ui->Tab_employe->setModel(C.afficher());
+ui->comboBox->addItems(C.lister());
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_bt_Ajouter_clicked()
+{
+    int Id =ui->lineEdit_Id->text().toInt() ;
+    QString Nom  = ui->lineEdit_Nom->text() ;
+
+    QString Email  = ui->lineEdit_Email->text() ;
+    int Tel =ui->lineEdit_Tel->text().toInt() ;
+
+    employe C(Id,Nom,Email,Tel) ;
+  bool test=C.ajouter();
+
+  if(test)
+  {
+      QMessageBox::information(nullptr, QObject::tr("Ok"),
+           QObject::tr("Ajout effectué.\n"
+                       "Click Cancel to exit."), QMessageBox::Cancel);
+
+      ui->Tab_employe->setModel(C.afficher())    ;
+      ui->comboBox->clear();
+      ui->comboBox->addItems(C.lister());
+
+  }
+  else
+  {
+      QMessageBox::information(nullptr, QObject::tr("Not Ok"),
+           QObject::tr("Ajout non effectué.\n"
+                       "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+  }
+
+
+}
+
+void MainWindow::on_Supp_Button_clicked()
+{
+
+
+    employe C1;
+   C1.set_id(ui->comboBox->currentText().toInt());
+
+
+   // if ( C1.verificationID()==0)
+
+   //{
+       bool test =C1.supprimer(C1.get_id())    ;
+   if(test)
+   {
+       QMessageBox::information(nullptr, QObject::tr("Ok"),
+            QObject::tr("Suppression effectué.\n"
+                        "Click Cancel to exit."), QMessageBox::Cancel);
+  ui->Tab_employe->setModel(C.afficher())    ;
+  ui->comboBox->clear();
+  ui->comboBox->addItems(C.lister());
+   }
+   else
+   {
+       QMessageBox::information(nullptr, QObject::tr("Not Ok"),
+            QObject::tr("Suppression non effectué.\n"
+                        "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+   }
+    //}
+   // else
+    //{
+      //  QMessageBox::warning(this, "Echec", "identifiant non existant !");
+
+
+    //}
+
+}
+
+
+
+void MainWindow::on_bt_Ajouter_md_clicked()
+{
+    int Id = ui->lineEdit_Id_md->text().toInt();
+    QString Nom= ui->lineEdit_Nom_md->text();
+     QString Mail= ui->lineEdit_Email_md->text();
+     int Tel = ui->lineEdit_Tel_md->text().toInt();
+
+     employe c(Id, Nom, Mail, Tel);
+    bool test=c.modifier(Id);
+    if(test)
+    {
+
+        ui->Tab_employe->setModel(c.afficher());//refresh
+
+               QMessageBox::information(nullptr, QObject::tr("effectué"),
+                    QObject::tr(" Modifié.\n"
+                                "Click Cancel to exit."), QMessageBox::Cancel);
+               ui->lineEdit_Id_md->clear();
+               ui->lineEdit_Nom_md->clear();
+               ui->lineEdit_Tel_md->clear();
+               ui->lineEdit_Email_md->clear();
+  }
+    else
+    {
+        QMessageBox::critical(nullptr, QObject::tr("non effectué"),
+                    QObject::tr("non modifié !.\n"
+                                "Click Cancel to exit."), QMessageBox::Cancel);
+        ui->lineEdit_Id_md->clear();
+        ui->lineEdit_Nom_md->clear();
+        ui->lineEdit_Tel_md->clear();
+        ui->lineEdit_Email_md->clear();
+
+
+
+    }
+
+}
---
+++ CRUD/mainwindow.h
@@ -0,0 +1,32 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include "employe.h"
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_bt_Ajouter_clicked();
+
+    void on_Supp_Button_clicked();
+
+    void on_bt_Ajouter_md_clicked();
+
+   // void on_bt_Modifier_2_clicked();
+    
+private:
+    Ui::MainWindow *ui;
+ employe C ;
+};
+
+#endif // MAINWINDOW_H
