ebfb282e54cfb092b3ab4dc846c2315fa448f74c
aziz306 <80386670+aziz306@users.noreply.github.com>
2021-11-15 22:08:48 +0100

    Add files via upload

---
+++ CRUD/employe.h
@@ -0,0 +1,42 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+#include <QString>
+#include <QSqlQueryModel>
+#include <QTableView>
+#include <QtWidgets>
+#include <QPrinter>
+#include<qcustomplot.h>
+class employe
+{
+    int Id ,Tel,age;
+    QString Nom , Email;
+public:
+    employe();
+     employe(int,QString,QString,int,int)    ;
+    int get_id();
+    int get_tel() ;
+    int get_age() ;
+
+    QString get_nom()  ;
+    QString get_email() ;
+    void set_id(int x)  ;
+    void set_tel(int x)  ;
+    void set_age(int x)  ;
+
+     void set_nom(QString ch)  ;
+    void set_email(QString ch)  ;
+    bool ajouter() ;
+    QSqlQueryModel *  afficher()  ;
+bool supprimer(int);
+bool chercherParCin()  ;
+int verificationID() ;
+bool modifier(int id)    ;
+QStringList lister();
+QSqlQueryModel * trierage();
+    QSqlQueryModel * trieralpha();
+void recherche(QTableView * tabl, QString,int,int);
+void pdf(QString filename);
+void stat(QCustomPlot *customPlot);
+};
+
+#endif //  employe_H
---
+++ CRUD/mainwindow.h
@@ -0,0 +1,50 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include "employe.h"
+#include <QMainWindow>
+#include "smtp.h"
+#include <QPrinter>
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+
+private slots:
+    void sendMail();
+    void mailSent(QString);
+    void on_bt_Ajouter_clicked();
+
+    void on_Supp_Button_clicked();
+
+    void on_bt_Ajouter_md_clicked();
+
+   // void on_bt_Modifier_2_clicked();
+    
+    void on_triage_clicked();
+
+    void on_trialphabetique_clicked();
+
+
+
+    void on_lineEdit_textChanged(const QString &arg1);
+
+    void on_pushButton_clicked();
+
+    void on_pushButton_8__clicked();
+
+    void on_stat_clicked();
+
+private:
+    Ui::MainWindow *ui;
+ employe C ;
+};
+
+#endif // MAINWINDOW_H
