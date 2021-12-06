c3ae99794f33ec7f13e1995d1ffe89ac420a17fc
aziz306 <80386670+aziz306@users.noreply.github.com>
2021-12-06 23:20:54 +0100

    Add files via upload

---
+++ CRUD/employe.cpp
@@ -0,0 +1,384 @@
+#include "employe.h"
+#include <QString>
+#include <QSqlQuery>
+#include <QtDebug>
+#include<QObject>
+#include <QTableView>
+#include <QPrinter>
+#include <QTextCursor>
+
+employe::employe()
+{
+    Id=0;
+
+    Nom="" ;
+    Email = "" ;
+    Tel = 0 ;
+    age=0;
+
+}
+
+employe::employe (int Id,QString Nom,QString Email,int Tel,int age)
+{
+this->Id=Id ;
+  this->Nom=Nom ;
+
+    this->Email=Email ;
+    this->Tel=Tel ;
+    this->age=age ;
+}
+int employe :: get_id()
+{
+    return Id ;
+}
+int  employe :: get_tel()
+{
+    return Tel ;
+}
+int  employe :: get_age()
+{
+    return age ;
+}
+QString  employe :: get_nom()
+{ return Nom ;
+}
+
+
+QString  employe :: get_email()
+{
+    return Email ;
+}
+void  employe :: set_id(int x)
+{
+   Id = x ;
+
+}
+void  employe :: set_age(int x)
+{
+   age = x ;
+
+}
+void  employe :: set_tel(int x)
+{
+    Tel = x ;
+}
+
+
+void  employe :: set_nom(QString ch)
+{
+    Nom = ch ;
+}
+void  employe :: set_email(QString ch)
+{
+
+    Email = ch ;
+
+}
+bool  employe ::  ajouter()
+{
+
+QSqlQuery query;
+QString id_string =QString::number(Id) ;
+
+query.prepare("INSERT INTO  employe (Id, Nom, Email,Tel,age) " " VALUES (:Id, :forename, :Email , :Tel,:age)");
+query.bindValue(":Id", id_string);
+query.bindValue(":forename", Nom);
+query.bindValue(":Email", Email);
+query.bindValue(":Tel", Tel);
+query.bindValue(":age", age);
+
+return query.exec() ;
+
+}
+
+QSqlQueryModel *  employe :: afficher()
+{
+    QSqlQueryModel *  model = new QSqlQueryModel ()    ;
+        model->setQuery("select * from  employe order by id");
+        model->setHeaderData(0, Qt::Horizontal,QObject :: tr("Identifiant"));
+        model->setHeaderData(1, Qt::Horizontal, QObject :: tr("Nom"));
+        model->setHeaderData(3, Qt::Horizontal, QObject :: tr("tel"));
+         model->setHeaderData(2, Qt::Horizontal, QObject :: tr("age"));
+        model->setHeaderData(4, Qt::Horizontal, QObject :: tr("Email"));
+
+return model ;
+}
+bool  employe :: supprimer(int)
+{
+    QSqlQuery query;
+
+    query.prepare("Delete from  employe where Id=:Id ") ;
+    query.bindValue(":Id",Id);
+
+
+
+    return query.exec() ;
+
+
+}
+
+bool  employe :: chercherParCin()
+{
+
+    QSqlQuery query;
+    query.prepare("Select * from  employe where Id=:Id");
+    query.bindValue(":Id",Id);
+    query.exec();
+
+    if (query.next())
+    {
+        Id=(query.value(0).toInt());
+       /*Nom=(query.value(1).toString());
+       Email=(query.value(2).toString());
+       Tel=(query.value(3).toInt());
+age=(query.value(4).toInt());*/
+
+    }
+    return query.exec();
+
+}
+
+
+int  employe ::verificationID()
+{
+  QSqlQuery query;
+
+  query.prepare("select * from  employe where Id=:Id");
+  query.bindValue(":Id",this->Id);
+  query.exec();
+
+  int count_user = 0;
+  while (query.next())
+  {
+      Id=(query.value(0).toInt());
+      Nom=(query.value(1).toString());
+      Email=(query.value(2).toString());
+      Tel=(query.value(3).toInt());
+      age=(query.value(4).toInt());
+      count_user++;
+  }
+  if (count_user==1)
+  {
+      return 0;
+  }
+  else if(count_user > 1)
+  {
+      return 1;
+  }
+  else
+  {
+      return 2;
+  }
+}
+
+
+
+bool  employe :: modifier(int id)
+{
+     QSqlQuery query;
+
+       // QString res= QString::number(identifiant);
+
+        QString res= QString::number(id);
+        query.prepare("UPDATE  employe SET  Nom=:Nom,Email=:Email,Tel=:Tel,age=:age WHERE Id=:Id");
+        query.bindValue(":Id", Id);
+        query.bindValue(":Nom", Nom);
+        query.bindValue(":Email", Email);
+        query.bindValue(":Tel", Tel);
+        query.bindValue(":age", age);
+
+
+
+           return    query.exec();
+
+}
+QStringList employe::lister(){
+QSqlQuery q;
+q.prepare("select id from employe");
+q.exec();
+QStringList l;
+while(q.next()){
+l<<q.value(0).toString();
+}
+return l;
+}
+QSqlQueryModel * employe::trierage()
+{
+    QSqlQueryModel * model=new QSqlQueryModel();
+    model->setQuery("SELECT * FROM EMPLOYE ORDER BY AGE");
+    model->setHeaderData(0, Qt::Horizontal,QObject :: tr("IDENTIFIANT"));
+    model->setHeaderData(1, Qt::Horizontal, QObject :: tr("NOM"));
+    model->setHeaderData(3, Qt::Horizontal, QObject :: tr("TEL"));
+     model->setHeaderData(2, Qt::Horizontal, QObject :: tr("AGE"));
+    model->setHeaderData(4, Qt::Horizontal, QObject :: tr("EMAIL"));
+    return model;
+}
+QSqlQueryModel * employe::trieralpha()
+{
+    QSqlQueryModel * model=new QSqlQueryModel();
+    model->setQuery("SELECT * FROM EMPLOYE ORDER BY NOM");
+    model->setHeaderData(0, Qt::Horizontal,QObject :: tr("IDENTIFIANT"));
+    model->setHeaderData(1, Qt::Horizontal, QObject :: tr("NOM"));
+    model->setHeaderData(3, Qt::Horizontal, QObject :: tr("TEL"));
+     model->setHeaderData(2, Qt::Horizontal, QObject :: tr("AGE"));
+    model->setHeaderData(4, Qt::Horizontal, QObject :: tr("EMAIL"));
+    return model;
+}
+void employe :: recherche(QTableView * table ,QString nom,int id, int age )
+{
+    QSqlQueryModel *model= new QSqlQueryModel();
+    QString id_string=QString::number(id);
+    QString age_string=QString::number(age);
+
+
+
+    QSqlQuery *query=new QSqlQuery;
+    query->prepare("select * from EMPLOYE where NOM like '%"+nom+"%' or ID like '%"+id_string+"%' or AGE like '%"+age_string+"%' ;");
+
+
+    query->exec();
+    model->setQuery(*query);
+    table->setModel(model);
+    table->show();
+
+}
+void pdf(QString filename)
+{
+    QPrinter printer(QPrinter::PrinterResolution);
+    printer.setOutputFormat(QPrinter::PdfFormat);
+    printer.setPaperSize(QPrinter::A4);
+    printer.setOutputFileName(filename);
+    printer.setPageMargins(QMarginsF(30, 30, 30, 30));
+
+    QFont headerFont("Times New Roman", 8);
+    QFont titleFont("Times New Roman", 14, QFont::Bold);
+
+    QTextCharFormat txtformat = QTextCharFormat();
+
+
+    QTextDocument doc;
+    doc.setPageSize(printer.pageRect().size());
+
+    QTextCursor* cursor = new QTextCursor(&doc);
+
+    txtformat.setFont(headerFont);
+    cursor->insertText("Company XYZ", txtformat);
+
+
+    doc.print(&printer);
+}
+void employe::stat(QCustomPlot *customPlot)
+{
+    QSqlQuery query,query1;
+    // set dark background gradient:
+    QLinearGradient gradient(0, 0, 0, 400);
+    gradient.setColorAt(0, QColor(90, 90, 90));
+    gradient.setColorAt(0.38, QColor(105, 105, 105));
+    gradient.setColorAt(1, QColor(70, 70, 70));
+    customPlot->clearPlottables();
+    customPlot->clearGraphs();
+    customPlot->replot();
+
+    customPlot->setBackground(QBrush(gradient));
+
+
+    QCPBars *fossil = new QCPBars(customPlot->xAxis, customPlot->yAxis);
+
+    fossil->setAntialiased(false);
+
+    fossil->setStackingGap(1);
+    // set names and colors:
+    fossil->setName("statistique des ages");
+    fossil->setPen(QPen(QColor(111, 9, 176).lighter(170)));
+    fossil->setBrush(QColor(111, 9, 176));
+
+    QVector<double> ticks;
+    QVector<QString> labels;
+    query.prepare("SELECT COUNT(DISTINCT id) FROM EMPLOYE where age between 20 and 30");
+    query.exec();
+    int un;
+    while(query.next())
+    {
+        un=query.value(0).toInt();
+        qDebug()<<un;
+    }
+    query.prepare("SELECT COUNT(DISTINCT id) FROM EMPLOYE where age between 31 and 45");
+    query.exec();
+    int deux;
+    while(query.next())
+    {
+        deux=query.value(0).toInt();
+    }
+
+    query.prepare("SELECT COUNT(DISTINCT id) FROM EMPLOYE where age between 46 and 60");
+    query.exec();
+    int trois;
+    while(query.next())
+    {
+        trois=query.value(0).toInt();
+    }
+
+
+
+
+
+    ticks << 1 << 2 << 3 ;
+    labels << "[20,30]" << "[31,45]" << "[46,60]" ;
+    QSharedPointer<QCPAxisTickerText> textTicker(new QCPAxisTickerText);
+    textTicker->addTicks(ticks, labels);
+    customPlot->xAxis->setTicker(textTicker);
+    customPlot->xAxis->setTickLabelRotation(60);
+    customPlot->xAxis->setSubTicks(false);
+    customPlot->xAxis->setTickLength(0, 4);
+    customPlot->xAxis->setRange(0, 8);
+    customPlot->xAxis->setBasePen(QPen(Qt::white));
+    customPlot->xAxis->setTickPen(QPen(Qt::white));
+    customPlot->xAxis->grid()->setVisible(true);
+    customPlot->xAxis->grid()->setPen(QPen(QColor(130, 130, 130), 0, Qt::DotLine));
+    customPlot->xAxis->setTickLabelColor(Qt::white);
+    customPlot->xAxis->setLabelColor(Qt::green);
+
+    // prepare y axis:
+    customPlot->yAxis->setRange(0, 20);
+    customPlot->yAxis->setPadding(5); // a bit more space to the left border
+    customPlot->yAxis->setLabel("Nombre de EMPLOYE");
+    customPlot->yAxis->setBasePen(QPen(Qt::white));
+    customPlot->yAxis->setTickPen(QPen(Qt::white));
+    customPlot->yAxis->setSubTickPen(QPen(Qt::white));
+    customPlot->yAxis->grid()->setSubGridVisible(true);
+    customPlot->yAxis->setTickLabelColor(Qt::white);
+    customPlot->yAxis->setLabelColor(Qt::white);
+    customPlot->yAxis->grid()->setPen(QPen(QColor(130, 130, 130), 0, Qt::SolidLine));
+    customPlot->yAxis->grid()->setSubGridPen(QPen(QColor(130, 130, 130), 0, Qt::DotLine));
+
+    // Add data:
+    QVector<double> fossilData, nuclearData, regenData;
+    fossilData  << un << deux << trois;
+   // nuclearData << 0.08*10.5 << 0.12*5.5 << 0.12*5.5 << 0.40*5.8 << 0.09*5.2 << 0.00*4.2 << 0.07*11.2;
+    regenData   << 0.06*10.5 << 0.05*5.5 << 0.04*5.5 << 0.06*5.8 << 0.02*5.2 << 0.07*4.2<< 0.25*11.2;
+    fossil->setData(ticks, fossilData);
+  //  nuclear->setData(ticks, nuclearData);
+ //   regen->setData(ticks, regenData);
+
+    // setup legend:
+    customPlot->legend->setVisible(true);
+    customPlot->axisRect()->insetLayout()->setInsetAlignment(0, Qt::AlignTop|Qt::AlignHCenter);
+    customPlot->legend->setBrush(QColor(255, 255, 255, 100));
+    customPlot->legend->setBorderPen(Qt::NoPen);
+    QFont legendFont = QFont();
+    legendFont.setPointSize(10);
+    customPlot->legend->setFont(legendFont);
+    customPlot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom);
+
+}
+QSqlQueryModel * employe::triid()
+{
+    QSqlQueryModel * model=new QSqlQueryModel();
+    model->setQuery("SELECT * FROM EMPLOYE ORDER BY ID");
+    model->setHeaderData(0, Qt::Horizontal,QObject :: tr("IDENTIFIANT"));
+    model->setHeaderData(1, Qt::Horizontal, QObject :: tr("NOM"));
+    model->setHeaderData(3, Qt::Horizontal, QObject :: tr("TEL"));
+     model->setHeaderData(2, Qt::Horizontal, QObject :: tr("AGE"));
+    model->setHeaderData(4, Qt::Horizontal, QObject :: tr("EMAIL"));
+    return model;
+}
---
+++ CRUD/mainwindow.cpp
@@ -0,0 +1,286 @@
+   #include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "employe.h"
+#include <QMessageBox>
+#include <QIntValidator>
+#include <QFileDialog>
+#include <QTableView>
+#include <QSqlQuery>
+#include <QPrinter>
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+  ui->lineEdit_Id->setValidator( new QIntValidator(0, 9999, this));
+ui->Tab_employe->setModel(C.afficher());
+ui->comboBox->addItems(C.lister());
+ui->comboBox_2->addItems(C.lister());
+connect(ui->sendBtn, SIGNAL(clicked()),this, SLOT(sendMail()));
+connect(ui->exitBtn, SIGNAL(clicked()),this, SLOT(close()));
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_bt_Ajouter_clicked()
+{
+    int Id =ui->lineEdit_Id->text().toInt() ;
+    QString Nom  = ui->lineEdit_Nom->text() ;
+
+    QString Email  = ui->lineEdit_Email->text() ;
+    int Tel =ui->lineEdit_Tel->text().toInt() ;
+    int age =ui->lineEdit_age->text().toInt() ;
+
+    employe C(Id,Nom,Email,Tel,age) ;
+  bool test=C.ajouter();
+
+  if(test)
+  {
+      QMessageBox::information(nullptr, QObject::tr("Ok"),
+           QObject::tr("Ajout effectué.\n"
+                       "Click Cancel to exit."), QMessageBox::Cancel);
+
+      ui->Tab_employe->setModel(C.afficher());
+      ui->comboBox->clear();
+      ui->comboBox->addItems(C.lister());
+      ui->comboBox_2->clear();
+      ui->comboBox_2->addItems(C.lister());
+
+  }
+  else
+  {
+      QMessageBox::information(nullptr, QObject::tr("Not Ok"),
+           QObject::tr("Ajout non effectué.\n"
+                       "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+  }
+
+
+}
+
+void MainWindow::on_Supp_Button_clicked()
+{
+
+
+    employe C1;
+   C1.set_id(ui->comboBox->currentText().toInt());
+
+
+   // if ( C1.verificationID()==0)
+
+   //{
+       bool test =C1.supprimer(C1.get_id())    ;
+   if(test)
+   {
+       QMessageBox::information(nullptr, QObject::tr("Ok"),
+            QObject::tr("Suppression effectué.\n"
+                        "Click Cancel to exit."), QMessageBox::Cancel);
+  ui->Tab_employe->setModel(C.afficher())    ;
+  ui->comboBox->clear();
+  ui->comboBox->addItems(C.lister());
+   }
+   else
+   {
+       QMessageBox::information(nullptr, QObject::tr("Not Ok"),
+            QObject::tr("Suppression non effectué.\n"
+                        "Click Cancel to exit."), QMessageBox::Cancel);
+
+
+   }
+    //}
+   // else
+    //{
+      //  QMessageBox::warning(this, "Echec", "identifiant non existant !");
+
+
+    //}
+
+}
+
+
+
+void MainWindow::on_bt_Ajouter_md_clicked()
+{
+
+    int Id =ui->comboBox_2->currentText().toInt();
+    QString Nom= ui->lineEdit_Nom_md->text();
+     QString Mail= ui->lineEdit_Email_md->text();
+     int Tel = ui->lineEdit_Tel_md->text().toInt();
+     int age = ui->lineEdit_age_md->text().toInt();
+
+     employe c(Id, Nom, Mail, Tel,age);
+    bool test=c.modifier(Id);
+    if(test)
+    {
+
+        ui->Tab_employe->setModel(c.afficher());//refresh
+
+               QMessageBox::information(nullptr, QObject::tr("effectué"),
+                    QObject::tr(" Modifié.\n"
+                                "Click Cancel to exit."), QMessageBox::Cancel);
+
+               ui->lineEdit_Nom_md->clear();
+               ui->lineEdit_Tel_md->clear();
+
+               ui->lineEdit_Email_md->clear();
+                ui->lineEdit_age_md->clear();
+  }
+    else
+    {
+        QMessageBox::critical(nullptr, QObject::tr("non effectué"),
+                    QObject::tr("non modifié !.\n"
+                                "Click Cancel to exit."), QMessageBox::Cancel);
+
+        ui->lineEdit_Nom_md->clear();
+        ui->lineEdit_Tel_md->clear();
+        ui->lineEdit_Email_md->clear();
+        ui->lineEdit_age_md->clear();
+
+
+
+    }
+
+}
+
+void MainWindow::sendMail()
+{
+    Smtp* smtp = new Smtp(ui->uname->text(), ui->paswd->text(), ui->server->text(), ui->port->text().toInt());
+    connect(smtp, SIGNAL(status(QString)), this, SLOT(mailSent(QString)));
+
+
+    smtp->sendMail(ui->uname->text(), ui->rcpt->text() , ui->subject->text(),ui->msg->toPlainText());
+}
+
+void MainWindow::mailSent(QString status)
+{
+    if(status == "Message sent")
+        QMessageBox::warning( 0, tr( "Qt Simple SMTP client" ), tr( "Message sent!\n\n" ) );
+}
+
+void MainWindow::on_triage_clicked()
+{
+    ui->Tab_employe->setModel(C.trierage());
+}
+
+void MainWindow::on_trialphabetique_clicked()
+{
+   ui->Tab_employe->setModel(C.trieralpha());
+}
+
+
+
+
+
+
+void MainWindow::on_lineEdit_textChanged(const QString &arg1)
+{
+    employe em;
+
+
+    QString nom = ui->lineEdit->text();
+    int age = ui->lineEdit->text().toInt();
+    int id= ui->lineEdit->text().toInt();
+    em.recherche(ui->Tab_employe,nom,id,age);
+    if (ui->lineEdit->text().isEmpty())
+    {
+        ui->Tab_employe->setModel(em.afficher());
+    }
+}
+
+
+void MainWindow::on_pushButton_8__clicked()
+{
+
+        QString strStream;
+                              QTextStream out(&strStream);
+
+                               const int rowCount = ui->Tab_employe->model()->rowCount();
+                               const int columnCount = ui->Tab_employe->model()->columnCount();
+                              out <<  "<html>\n"
+                              "<head>\n"
+                                               "<meta Content=\"Text/html; charset=Windows-1251\">\n"
+                                               <<  QString("<title>%1</title>\n").arg("strTitle")
+                                               <<  "</head>\n"
+                                               "<body bgcolor=#ffffff link=#5000A0>\n"
+
+                                              //     "<align='right'> " << datefich << "</align>"
+                                               "<center> <H1>Liste des Employes</H1></br></br><table border=1 cellspacing=0 cellpadding=2>\n";
+
+                                           // headers
+                                           out << "<thead><tr bgcolor=#f0f0f0> <th>Numero</th>";
+                                           out<<"<cellspacing=10 cellpadding=3>";
+                                           for (int column = 0; column < columnCount; column++)
+                                               if (!ui->Tab_employe->isColumnHidden(column))
+                                                   out << QString("<th>%1</th>").arg(ui->Tab_employe->model()->headerData(column, Qt::Horizontal).toString());
+                                           out << "</tr></thead>\n";
+
+                                           // data table
+                                           for (int row = 0; row < rowCount; row++) {
+                                               out << "<tr> <td bkcolor=0>" << row+1 <<"</td>";
+                                               for (int column = 0; column < columnCount; column++) {
+                                                   if (!ui->Tab_employe->isColumnHidden(column)) {
+                                                       QString data = ui->Tab_employe->model()->data(ui->Tab_employe->model()->index(row, column)).toString().simplified();
+                                                       out << QString("<td bkcolor=0>%1</td>").arg((!data.isEmpty()) ? data : QString("&nbsp;"));
+                                                   }
+                                               }
+                                               out << "</tr>\n";
+                                           }
+                                           out <<  "</table> </center>\n"
+                                               "</body>\n"
+                                               "</html>\n";
+
+                                     QString fileName = QFileDialog::getSaveFileName((QWidget* )0, "Sauvegarder en PDF", QString(), "*.pdf");
+                                       if (QFileInfo(fileName).suffix().isEmpty()) { fileName.append(".pdf"); }
+
+                                      QPrinter printer (QPrinter::PrinterResolution);
+                                       printer.setOutputFormat(QPrinter::PdfFormat);
+                                      printer.setPaperSize(QPrinter::A4);
+                                     printer.setOutputFileName(fileName);
+
+                                      QTextDocument doc;
+                                       doc.setHtml(strStream);
+                                       doc.setPageSize(printer.pageRect().size()); // This is necessary if you want to hide the page number
+                                       doc.print(&printer);
+    }
+
+
+
+void MainWindow::on_stat_clicked()
+{
+    ui->tabWidget->setCurrentIndex(3);
+    employe E;
+
+
+    E.stat(ui->widget);
+}
+
+void MainWindow::on_comboBox_2_currentIndexChanged(const QString &arg1)
+{
+    QString id =ui->comboBox_2->currentText();
+
+
+
+              QSqlQuery e;
+              e.prepare("select * from EMPLOYE where id ='"+id+"'");
+             if(e.exec())
+             {
+                 while (e.next()) {
+                      ui->lineEdit_Nom_md->setText(e.value(1).toString());
+                      ui->lineEdit_Email_md->setText(e.value(0).toString());
+
+                      ui->lineEdit_Tel_md->setText(e.value(3).toString());
+                      ui->lineEdit_age_md->setText(e.value(2).toString());
+
+                 }
+             }
+}
+
+void MainWindow::on_triid_clicked()
+{
+      ui->Tab_employe->setModel(C.triid());
+}
